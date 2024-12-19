2cfb4a1298d5935daa5edbf96dc354985052af06
Oladipupo <dipoakanji57@gmail.com>
2024-12-19 08:11:55 +0100

    Added timeout function to remove inputted values

--- Typescript-Pr/src/modules/module.ts
+++ Typescript-Pr/src/modules/module.ts
@@ -1,4 +1,3 @@
-
 export let usernameInput: HTMLInputElement = document.getElementById('username') as HTMLInputElement;
 export let useremailInput: HTMLInputElement = document.getElementById('useremail') as HTMLInputElement
 export let userpasswordInput: HTMLInputElement = document.getElementById('userpassword') as HTMLInputElement
@@ -6,3 +5,4 @@ export let result: HTMLParagraphElement = document.getElementById('result') as H
 export let result1: HTMLParagraphElement = document.getElementById('result1') as HTMLParagraphElement
 export let result2: HTMLParagraphElement = document.getElementById('result2') as HTMLParagraphElement
 export let btn: HTMLButtonElement = document.getElementById("submitBtn") as HTMLButtonElement
+export let div: HTMLDivElement = document.getElementById("append-div") as HTMLDivElement
